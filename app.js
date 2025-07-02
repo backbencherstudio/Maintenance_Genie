1bbb2930d637cd3f8543c86201a23d1858b41841
kamranhossaintopu <hossainarman953@gmail.com>
2025-07-02 14:47:28 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const { prisma, connectDB } = require('./modules/prisma/prisma');
+const userRoutes = require('./modules/user/user.route');
+const path = require('path');
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Connect to the database
+connectDB();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from the "uploads" folder
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+
+app.get('/', (req, res) => {
+  res.send('🚀 Streamly API is running');
+});
+
+// User-related routes (e.g., /register, /login)
+app.use('/api/users', userRoutes); // 👈 cleaner URL like /api/users/register
+
+
+// Global 404 fallback
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
+});
---
+++ prisma/migrations/20250702082753_/migration.sql
@@ -0,0 +1,32 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('active', 'inactive', 'expired');
+
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('normal', 'premium', 'admin');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "password" TEXT,
+    "customer_id" TEXT,
+    "country" TEXT,
+    "gender" TEXT,
+    "status" "Status" NOT NULL DEFAULT 'active',
+    "role" "Role" NOT NULL DEFAULT 'normal',
+    "date_of_birth" DATE,
+    "city" TEXT,
+    "phone_number" TEXT,
+    "state" TEXT,
+    "postal_code" TEXT,
+    "bio" TEXT,
+    "is_two_factor_enabled" INTEGER DEFAULT 0,
+    "two_factor_secret" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
